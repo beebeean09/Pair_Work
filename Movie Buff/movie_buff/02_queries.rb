3bb0f0aa568c84a87de4ea09e4166716fd795809
vivian_lee <beebeean09@gmail.com>
2017-02-09 13:55:15 -0800

    Complete 2nd seq

--- Movie Buff/movie_buff/02_queries.rb
+++ Movie Buff/movie_buff/02_queries.rb
@@ -30,5 +30,5 @@ end
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-  Actor.select(:id, :name).joins(:castings).group(:id).where("castings.ord != '1'").limit(2)
+  Actor.select(:id, :name, "COUNT(movie_id) AS roles").joins(:castings).group(:id).where("castings.ord != '1'").order("roles DESC").limit(2)
 end
